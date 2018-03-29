7c30b26f0dc122e7e084eb2126dd5c028fcb8495
Matthew Tebbs <matthew@muddytummy.com>
2018-03-29 14:10:05 -0700

    Tweak order.

--- sim/_helper.ts
+++ sim/_helper.ts
@@ -6,19 +6,18 @@
 
 namespace pxsim {
     export class Helper {
-        public static safeAmmoDestroy(object: any) {
-            if (object) {
-                Ammo.destroy(object);
-            }
-        }
-
         public static btVector3FromThree(vec: THREE.Vector3): Ammo.btVector3 {
             return new Ammo.btVector3(vec.x, vec.y, vec.z);
         }
 
         public static btQuaternionFromThree(qtr: THREE.Quaternion): Ammo.btQuaternion {
             return new Ammo.btQuaternion(qtr.x, qtr.y, qtr.z, qtr.w);
+        }
 
+        public static safeAmmoDestroy(object: any) {
+            if (object) {
+                Ammo.destroy(object);
+            }
         }
     }
 }
