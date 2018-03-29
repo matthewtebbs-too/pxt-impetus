21e9cdc93c438f0e035abce1755b746ef9232302
Matthew Tebbs <matthew@muddytummy.com>
2018-03-29 11:06:55 -0700

    Remove git ignored files.

--- built/packaged/pxt-Impetus/docs/about.md
+++
@@ -1,3 +0,0 @@
-# About Impetus
-
-See [GitHub repo](https://github.com/muddytummy/pxt-Impetus.git) for details.
--- built/pxtrequire.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../node_modules/pxt-core/built/pxtlib.d.ts" />
-export declare let pxtCore: any;
-export declare let pxtCoreDir: string;
