95128d47d75f08597da8df33b99a3bb43eb7ab57
Matthew Tebbs <matthew@muddytummy.com>
2018-04-22 15:45:38 -0700

    Fix example projects to add perspective camera.

--- libs/blocksprj/main.ts
+++ libs/blocksprj/main.ts
@@ -1,1 +1,4 @@
- 
+world.scene().camera = design.perspectiveCamera()
+world.scene().camera.position = Math.vector(0, 20, 20)
+world.scene().camera.lookAtPosition(Math.zeroVector())
+world.scene().addAt(design.directionalLight(design.colorPicker(0xffffff), 1), Math.vector(5, 20, 0))
